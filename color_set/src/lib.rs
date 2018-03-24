17da57c66e59ee9bede7e424da84821aec14771d
rademonster <sorenrade@gmail.com>
2018-03-24 18:11:02 -0400

    Inital commit

---
+++ color_set/src/lib.rs
@@ -0,0 +1,89 @@
+extern crate palette;
+use palette::{Hsv, IntoColor};
+use palette::rgb::{Linear, Rgb};
+
+use std::fmt::Debug;
+use std::collections::hash_map::DefaultHasher;
+use std::hash::Hasher;
+
+pub type Color = (f32, f32, f32);
+
+const GOLDEN_RATIO_CONJUGATE: f64 = 0.618033988749895;
+#[derive(Debug)]
+struct Generator {
+    theta: f64,
+    pub sat: f32,
+    pub val: f32
+}
+
+fn get_angle<T: Debug>(arg: T) -> f32 {
+    let s = format!("{:?}", arg);
+    let mut hasher = DefaultHasher::new();
+    for c in s.chars(){
+		hasher.write_u8(c as u8);
+	}
+	let mut ang = hasher.finish() as f64;
+	ang = ang / 10f64.powf(ang.log10().floor()+1.);
+	println!("val hashes to ang: {:?} {:?}", ang, ang as f32);
+	ang as f32
+}
+
+impl Generator{
+	pub fn new() -> Generator{
+		Generator{theta:0., sat: 0.9, val:0.9}
+	}
+	pub fn new_seed<T: Debug>(seed: T) -> Generator {
+	    let mut g = Generator::new();
+	    g.theta = get_angle(seed) as f64;
+	    g
+	}
+	pub fn get_color<T: Debug>(&self, seed: T) -> Color{
+		self.angle_to_color(get_angle(seed))
+	}
+	fn angle_to_color(&self, seed: f32) -> Color{
+		let c = Hsv::new(palette::RgbHue::from_radians(2.*3.14159*(seed as f32)) , self.sat, self.val);
+		let r: Rgb<Linear> = c.into_rgb();
+		(r.red, r.green, r.blue)
+		// hsv_to_rgb(360.*(seed as f32) , self.sat, self.val)
+	}
+}
+
+impl Iterator for Generator {
+	type Item = Color;
+    fn next(&mut self) -> Option<Color> {
+        let c = self.angle_to_color(self.theta as f32);
+        self.theta += (self.theta + GOLDEN_RATIO_CONJUGATE) % 1.;
+        return Some(c);
+    }
+}
+
+
+fn hsv_to_rgb(h: f32, s: f32, v: f32) -> Color{
+    let hi = ((h / 60.0) % 6.).floor();
+    let f =  (h / 60.0) - (h / 60.0).floor();
+    let p = v * (1.0 - s);
+    let q = v * (1.0 - (f*s));
+    let t = v * (1.0 - ((1.0 - f) * s));
+    match hi as usize{
+        0 => (v, t, p),
+        1 => (q, v, p),
+        2 => (p, v, t),
+        3 => (p, q, v),
+        4 => (t, p, v),
+        5 => (v, p, q),
+        _ => panic!("n mod 6 > 6")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+    #[test]
+    fn sanity() {
+        for i in Generator::new_seed(0).take(100){
+        	println!("{:?}", i);
+        }
+        println!("What color is this sentence? Its: {:?}", Generator::new().get_color("What color is this sentence?"));
+        assert!(true);
+    }
+}
---
+++ color_set/test.py
@@ -0,0 +1,38 @@
+from random import uniform
+from math import floor
+
+def hsvToRGB(h, s, v):
+    """Convert HSV color space to RGB color space
+    
+    @param h: Hue
+    @param s: Saturation
+    @param v: Value
+    return (r, g, b)  
+    """
+    hi = floor(h / 60.0) % 6
+    f =  (h / 60.0) - floor(h / 60.0)
+    p = v * (1.0 - s)
+    q = v * (1.0 - (f*s))
+    t = v * (1.0 - ((1.0 - f) * s))
+    return {
+        0: (v, t, p),
+        1: (q, v, p),
+        2: (p, v, t),
+        3: (p, q, v),
+        4: (t, p, v),
+        5: (v, p, q),
+    }[hi]
+
+
+theta = uniform(0,1)
+def next():
+    # use golden ratio
+    global theta
+    golden_ratio_conjugate = 0.618033988749895
+    theta += golden_ratio_conjugate
+    theta %= 1
+    return hsvToRGB(theta*360., .9, .9)
+
+
+for i in xrange(100):
+    print next()
